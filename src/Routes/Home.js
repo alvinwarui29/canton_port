3736af5a9e9a84581ee55a5ac642c6c044645d8b
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-29 01:38:11 +0300

    add components to route file

---
+++ src/Routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './../Components/Navbar/Navbar';
+import About from './../Components/About/About';
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <About/>
+    </div>
+  )
+}
+
+export default Home
