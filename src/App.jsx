597ac050ff48a02643fa7d100c63b1ed68bd4bc4
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-29 01:36:06 +0300

    finish add routes code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom;'
 function App() {
 
   return (
-    <div className="app"></div>
+    <div className="app">
+      <BrowserRouter>
+        {/* navbar */}
+
+        <Routes></Routes>
+
+
+      </BrowserRouter>
+    </div>
   )
 }
 
 export default App
+ 
