94abaabeb9d8a3bc01f0ac7b2e3aa2b030699b16
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-29 02:03:19 +0300

    routing complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
           <Routes>
-          <Route to="/" element={<Home/>}/>
+          <Route path="/" element={<Home/>}/>
           </Routes>
 
 
