bc2a24fced4f6034bc70520fc96949cd89d74e55
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-29 05:05:51 +0300

    wiring components to routes

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import './Hero.css'
+
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar';
 import About from '../Components/About/About';
+import Hero from '../Components/Hero/Hero';
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Hero/>
         <About/>
         
     </div>
