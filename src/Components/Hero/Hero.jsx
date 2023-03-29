091170fddcea320044280ac34f23cf889f1241cf
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-29 05:58:58 +0300

    finish design of jsx file

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,19 @@
 import './Hero.css'
-
+import Pic from '../../assets/pic.jpg'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero">
+        <div className="left-h">
+            <h3>Hello I am Alvin Maina alias Canton</h3>
+            <p>I'm a front-end developer</p>
+            <p>I'm also well acquainted in front-end developer</p>
+        </div>
+        <div className="right-h">
+            <img src={Pic} alt="" />
+        </div>
+    </div>
   )
 }
 
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div>
         <Navbar/>
         <Hero/>
-        <About/>
+        {/* <About/> */}
         
     </div>
   )
